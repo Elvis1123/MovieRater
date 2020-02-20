2b75eb21180e52e6b7dd0d7a0ba9a3b46e32cf1f
Elvis1123 <esehic2@etf.unsa.ba>
2020-02-20 21:46:05 +0100

    work in progress 3

--- api/serializers.py
+++ api/serializers.py
@@ -1,30 +1,34 @@
 from rest_framework import serializers
-from .models import Movie,Rating
+from .models import Movie, Rating
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
-'''class UserSerializer(serializers.ModelSerializer):
 
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = [ 'username']'''
+        fields = ['id', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
+
 
 class RatinSerilazier(serializers.ModelSerializer):
-    '''user = UserSerializer(many=False)'''
+    # user = UserSerializer(many=False)
 
     class Meta:
         model = Rating
-        fields = ['id', 'stars','user','movie']
+        fields = ['id', 'stars', 'user', 'movie']
 
 
 class MovieSerializer(serializers.ModelSerializer):
-    rating = RatinSerilazier(many=True)
+    # rating = RatinSerilazier(many=True)
 
     class Meta:
+        # ,'rating'
 
         model = Movie
-        fields = ['id','title', 'description','votes','rating','average_rate']
-
-
-
-
-
+        fields = ['id', 'title', 'description', 'average_rate', 'votes']
--- api/views.py
+++ api/views.py
@@ -1,27 +1,44 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from .serializers import MovieSerializer, RatinSerilazier
+from .serializers import MovieSerializer, RatinSerilazier, UserSerializer
 from .models import Movie, Rating
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.contrib.auth.models import User
 
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
 
 class RatingViewSet(viewsets.ModelViewSet):
     serializer_class = RatinSerilazier
     queryset = Rating.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
+    def update(self, request, *args, **kwargs):
+        response = {'message': 'You cant update rating like that'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    def create(self, request, *args, **kwargs):
+        response = {'message': 'You cant creating rating like that'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 class MovieViewSet(viewsets.ModelViewSet):
     serializer_class = MovieSerializer
     queryset = Movie.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     @action(detail=True, methods=['POST'])
     def rate_movie(self, request, pk=None):
         if 'stars' in request.data:
             movie = Movie.objects.get(id=pk)
             stars = request.data['stars']
-            # user = request.user
-            user = User.objects.get(id=1)
+            user = request.user
+
 
             try:
                 rating = Rating.objects.get(user=user.id, movie=movie.id)
@@ -31,7 +48,6 @@ class MovieViewSet(viewsets.ModelViewSet):
                 response = {'message': 'Rating updated', 'result': serializer.data }
                 return Response(response, status=status.HTTP_200_OK)
             except:
-                print("I am stupid")
                 rating = Rating.objects.create(user=user, movie=movie, stars=stars)
                 serializer = RatinSerilazier(rating, many=False)
                 response = {'message': 'Rating created', 'result': serializer.data}
@@ -40,3 +56,5 @@ class MovieViewSet(viewsets.ModelViewSet):
         else:
             response = {'message': 'You need to provide stars'}
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+
